07c3b48d1c65575e6318435df92b5972bb74af0e
Pascal Schardt (M. Waechter) <pschardt@pcgris-l112.igd.fraunhofer.de>
2014-11-25 16:38:32 +0100

    libs/dmrecon

--- libs/util/quadrangle.cc
+++ libs/util/quadrangle.cc
@@ -24,7 +24,7 @@ Quadrangle::Quadrangle()
   bool Quadrangle::isInside( int x, int y)
   {
     // Check weather the quadrangle has four corners.
-    if( isComplete() )
+    /*if( isComplete() )
     {
       unsigned int i, j; 
       bool c = false;
@@ -35,7 +35,31 @@ Quadrangle::Quadrangle()
       }
       return c;
     }
-    return false;
+    return false;*/
+
+	if (((xCoords[1] - xCoords[0]) * (y - yCoords[0]) - (x - xCoords[0]) * (yCoords[1] - yCoords[0])) <= 0) {
+	if (((xCoords[2] - xCoords[1]) * (y - yCoords[1]) - (x - xCoords[1]) * (yCoords[2] - yCoords[1])) <= 0) {
+	if (((xCoords[3] - xCoords[2]) * (y - yCoords[2]) - (x - xCoords[2]) * (yCoords[3] - yCoords[2])) <= 0) {
+	if (((xCoords[0] - xCoords[3]) * (y - yCoords[3]) - (x - xCoords[3]) * (yCoords[0] - yCoords[3])) <= 0) {
+	return true;
+}
+}
+}
+}
+
+	if (((xCoords[1] - xCoords[0]) * (y - yCoords[0]) - (x - xCoords[0]) * (yCoords[1] - yCoords[0])) >= 0) {
+	if (((xCoords[2] - xCoords[1]) * (y - yCoords[1]) - (x - xCoords[1]) * (yCoords[2] - yCoords[1])) >= 0) {
+	if (((xCoords[3] - xCoords[2]) * (y - yCoords[2]) - (x - xCoords[2]) * (yCoords[3] - yCoords[2])) >= 0) {
+	if (((xCoords[0] - xCoords[3]) * (y - yCoords[3]) - (x - xCoords[3]) * (yCoords[0] - yCoords[3])) >= 0) {
+	return true;
+}
+}
+}
+}
+
+return false;
+
+
   }
   
   void Quadrangle::addPoint( int x, int y)
