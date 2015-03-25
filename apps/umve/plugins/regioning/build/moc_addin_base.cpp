db4756934cb57a50573ac545c66de8a6c195921f
Pascal <pascal@pascal-ms-7850.com>
2015-03-25 13:14:46 +0100

    Final bugfixed and code cleanup

--- apps/umve/plugins/regioning/build/moc_addin_base.cpp
+++ apps/umve/plugins/regioning/build/moc_addin_base.cpp
@@ -1,17 +1,16 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'addin_base.h'
 **
-** Created: Wed Mar 18 11:49:54 2015
-**      by: The Qt Meta Object Compiler version 63 (Qt 4.8.1)
+** Created by: The Qt Meta Object Compiler version 63 (Qt 4.8.6)
 **
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
 
-#include "../../../../../../mve/apps/umve/scene_addins/addin_base.h"
+#include "../../../scene_addins/addin_base.h"
 #if !defined(Q_MOC_OUTPUT_REVISION)
 #error "The header file 'addin_base.h' doesn't include <QObject>."
 #elif Q_MOC_OUTPUT_REVISION != 63
-#error "This file was generated using the moc from 4.8.1. It"
+#error "This file was generated using the moc from 4.8.6. It"
 #error "cannot be used with the include files from this version of Qt."
 #error "(The moc has changed too much.)"
 #endif
